28e590249ae05ab0be1fd8d05c49a19f690ccc00
Carlos Figueiredo <carlos.figueiredo87@gmail.com>
2015-07-19 23:52:24 -0300

    Add api pipeline

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,3 +1,15 @@
 defmodule HelloPhoenix.UserView do
   use HelloPhoenix.Web, :view
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id}
+  end
 end
